c011fd5ae4d224dd2cf856b0b2538b373a5f1437
jabezinu <jabezzinu31@gmail.com>
2025-05-30 01:51:43 +0300

    done with the Edit page

--- frontend/src/pages/EditBooks.jsx
+++ frontend/src/pages/EditBooks.jsx
@@ -1,11 +1,76 @@
-import React from 'react'
+import React from'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import {Link, useNavigate, useParams} from 'react-router-dom'
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-const EditBooks = () => {
+const EditBook = () => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [publishedYear, setPublishedYear] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const {id} = useParams();
+  useEffect(() => {
+    setLoading(true);
+    axios.get(`http://localhost:5555/books/${id}`)
+    .then((res) => {
+      setAuthor(res.data.author)
+      setPublishedYear(res.data.publishedYear)
+      setTitle(res.data.title)
+      setLoading(false)
+    })
+    .catch((err) => {
+      setLoading(false);
+      alert('An error happened. Please Check console')
+      console.log(err)
+    })
+  }, [])
+  const handleEditBook = () => {
+    const data = {
+      title,
+      author,
+      publishedYear,
+    };
+    setLoading(true);
+    axios
+      .put(`http://localhost:5555/books/${id}`, data)
+      .then(() => {
+        setLoading(false);
+        navigate('/')
+      })
+      .catch((err) => {
+        setLoading(false);
+        alert('An error happened. Please Check console');
+        console.log(err);
+      })
+
+  }
+  
   return (
-    <div>
-      Edite
+    <div className='p-4'>
+      <BackButton />
+        <h1 className='text-3xl my-4'>Edite Book</h1>
+        {loading ? <Spinner /> : ''}
+        <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+          <div className='my-4'>
+            <label className='text-xl mr-4 text-gray-500'>Title</label>
+            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} className='border-2 border-gray-500 px-4 py-2 w-full'/>
+            
+            <label className='text-xl mr-4 text-gray-500'>Author</label>
+            <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} className='border-2 border-gray-500 px-4 py-2 w-full'/>
+            
+            <label className='text-xl mr-4 text-gray-500'>Publisher</label>
+            <input type="number" value={publishedYear} onChange={(e) => setPublishedYear(e.target.value)} className='border-2 border-gray-500 px-4 py-2 w-full'/>
+            
+            <button className='p-2 bg-sky-300 m-8' onClick={handleEditBook}>Update</button>
+            <Link to={'/'}><button className='p-2 bg-gray-300 m-8' >Cancle</button></Link>
+          </div>
+        </div>
     </div>
   )
 }
 
-export default EditBooks
+export default EditBook
