85baff167e535356151eb9eb8d4bca69f27e57c2
jabezinu <jabezzinu31@gmail.com>
2025-05-29 12:10:24 +0300

    install mongoose and create connection to the DB

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
-import {PORT} from "./config.js";
+import {PORT, MONGO_URI} from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -7,6 +8,15 @@ app.get('/', (req, res) => {
     return res.status(234).send("Welcome to MERN Stack Tutorial.")
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`)
-})
+
+mongoose
+    .connect(MONGO_URI)
+    .then(() => {
+        console.log("App connected to database")
+        app.listen(PORT, () => {
+            console.log(`Server is running on port: ${PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log(err)
+    });
