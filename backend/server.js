e3edaf9f64c4bf6c90fce26a7bc57a6c114cf442
jabezinu <jabezzinu31@gmail.com>
2025-05-29 12:52:04 +0300

    add Get all books method

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,22 @@ app.post('/books', async (req, res) => {
 })
 
 
+// route for Get All Books from database
+app.get("/books", async (req, res) => {
+    try {
+        const books = await Book.find({})
+
+        return res.status(200).json({
+            count: books.length,
+            data: books
+        })
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send({message: error.message})        
+    }
+})
+
+
 
 mongoose
     .connect(MONGO_URI)
