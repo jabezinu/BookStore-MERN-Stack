a7483ad62063dce0c0ca62fa0c0b2ac8ee0ba28a
jabezinu <jabezzinu31@gmail.com>
2025-05-30 02:18:10 +0300

    done with the delete page

--- frontend/src/pages/DeleteBooks.jsx
+++ frontend/src/pages/DeleteBooks.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { useState } from 'react';
 
 const DeleteBooks = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const {id} = useParams();
+
+  const handleDeleteBook = () => {
+    setLoading(true)
+    axios
+      .delete(`http://localhost:5555/books/${id}`)
+      .then(() => {
+        setLoading(false);
+        navigate('/');
+      })
+      .catch((err) => {
+        setLoading(false);
+        alert('An error happend. Pleas check console');
+        console.log(err)
+      })
+  }
   return (
-    <div>
-      Delete
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Delete Book</h1>
+      {loading ? <Spinner /> : ''}
+      <div className='flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto'>
+        <h3 className='text-2xl'>Are You Sure You Want To Delete This Book From The List?!</h3>
+
+        <button className='p-4 bg-red-600 text-white m-8 w-full' onClick={handleDeleteBook}>Yes, Delete it</button>
+        <Link className='p-4 bg-gray-400 text-white m-8 w-full' to={'/'}><button>No, Don't</button></Link>
+      </div>
     </div>
   )
 }
